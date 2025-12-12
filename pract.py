0adbbd3d5cc0be5654c5706ccde82274764b2eb2
Subham Patra <subhampatra2605@gmail.com>
2025-12-12 23:41:39 +0530

    Some more linked list examples

--- pract.py
+++ pract.py
@@ -273,16 +273,157 @@
 #     ll.remove_by_value("grapes")
 #     ll.print()
 
+# class Node:
+#     def __init__(self, data=None, next=None):
+#         self.data = data
+#         self.next = next
+
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
+
+#     def print(self):
+#         if self.head is None:
+#             print("Linked list is empty")
+#             return
+
+#         itr = self.head
+#         llstr = ''
+#         while itr:
+#             llstr += str(itr.data) + ' --> '
+#             itr = itr.next
+#         print(llstr)
+
+#     def get_length(self):
+#         count = 0
+#         itr = self.head
+#         while itr:
+#             count+=1
+#             itr = itr.next
+
+#         return count
+
+#     def insert_at_begining(self, data):
+#         node = Node(data, self.head)
+#         self.head = node
+
+#     def insert_at_end(self, data):
+#         if self.head is None:
+#             self.head = Node(data, None)
+#             return
+
+#         itr = self.head
+
+#         while itr.next:
+#             itr = itr.next
+
+#         itr.next = Node(data, None)
+
+#     def insert_at(self, index, data):
+#         if index<0 or index>self.get_length():
+#             raise Exception("Invalid Index")
+
+#         if index==0:
+#             self.insert_at_begining(data)
+#             return
+
+#         count = 0
+#         itr = self.head
+#         while itr:
+#             if count == index - 1:
+#                 node = Node(data, itr.next)
+#                 itr.next = node
+#                 break
+
+#             itr = itr.next
+#             count += 1
+
+#     def remove_at(self, index):
+#         if index<0 or index>=self.get_length():
+#             raise Exception("Invalid Index")
+
+#         if index==0:
+#             self.head = self.head.next
+#             return
+
+#         count = 0
+#         itr = self.head
+#         while itr:
+#             if count == index - 1:
+#                 itr.next = itr.next.next
+#                 break
+
+#             itr = itr.next
+#             count+=1
+
+#     def insert_values(self, data_list):
+#         self.head = None
+#         for data in data_list:
+#             self.insert_at_end(data)
+
+
+#     def insert_after_value(self, data_after, data_to_insert):
+#         if self.head is None:
+#             return
+
+#         if self.head.data==data_after:
+#             self.head.next = Node(data_to_insert,self.head.next)
+#             return
+
+#         itr = self.head
+#         while itr:
+#             if itr.data == data_after:
+#                 itr.next = Node(data_to_insert, itr.next)
+#                 break
+
+#             itr = itr.next
+
+#     def remove_by_value(self, data):
+#         if self.head is None:
+#             return
+
+#         if self.head.data == data:
+#             self.head = self.head.next
+#             return
+
+#         itr = self.head
+#         while itr.next:
+#             if itr.next.data == data:
+#                 itr.next = itr.next.next
+#                 break
+#             itr = itr.next
+
+# if __name__ == '__main__':
+#     ll = LinkedList()
+#     ll.insert_values(["banana","mango","grapes","orange"])
+#     ll.print()
+#     ll.insert_after_value("mango","apple")
+#     ll.print()
+#     ll.remove_by_value("orange")
+#     ll.print()
+#     ll.remove_by_value("figs")
+#     ll.print()
+#     ll.remove_by_value("banana")
+#     ll.remove_by_value("mango")
+#     ll.remove_by_value("apple")
+#     ll.remove_by_value("grapes")
+#     ll.print()
+
+#     # ll.insert_values([45,7,12,567,99])
+#     # ll.insert_at_end(67)
+#     # ll.print()
+
 class Node:
-    def __init__(self, data=None, next=None):
+    def __init__(self, data=None, next=None, prev=None):
         self.data = data
         self.next = next
+        self.prev = prev
 
-class LinkedList:
+class DoublyLinkedList:
     def __init__(self):
         self.head = None
 
-    def print(self):
+    def print_forward(self):
         if self.head is None:
             print("Linked list is empty")
             return
@@ -294,6 +435,26 @@ class LinkedList:
             itr = itr.next
         print(llstr)
 
+    def print_backward(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+
+        last_node = self.get_last_node()
+        itr = last_node
+        llstr = ''
+        while itr:
+            llstr += itr.data + '-->'
+            itr = itr.prev
+        print("Link list in reverse: ", llstr)
+
+    def get_last_node(self):
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+
+        return itr
+
     def get_length(self):
         count = 0
         itr = self.head
@@ -304,12 +465,17 @@ class LinkedList:
         return count
 
     def insert_at_begining(self, data):
-        node = Node(data, self.head)
-        self.head = node
+        if self.head == None:
+            node = Node(data, self.head, None)
+            self.head = node
+        else:
+            node = Node(data, self.head, None)
+            self.head.prev = node
+            self.head = node
 
     def insert_at_end(self, data):
         if self.head is None:
-            self.head = Node(data, None)
+            self.head = Node(data, None, None)
             return
 
         itr = self.head
@@ -317,7 +483,7 @@ class LinkedList:
         while itr.next:
             itr = itr.next
 
-        itr.next = Node(data, None)
+        itr.next = Node(data, None, itr)
 
     def insert_at(self, index, data):
         if index<0 or index>self.get_length():
@@ -331,7 +497,9 @@ class LinkedList:
         itr = self.head
         while itr:
             if count == index - 1:
-                node = Node(data, itr.next)
+                node = Node(data, itr.next, itr)
+                if node.next:
+                    node.next.prev = node
                 itr.next = node
                 break
 
@@ -344,13 +512,16 @@ class LinkedList:
 
         if index==0:
             self.head = self.head.next
+            self.head.prev = None
             return
 
         count = 0
         itr = self.head
         while itr:
-            if count == index - 1:
-                itr.next = itr.next.next
+            if count == index:
+                itr.prev.next = itr.next
+                if itr.next:
+                    itr.next.prev = itr.prev
                 break
 
             itr = itr.next
@@ -362,54 +533,17 @@ class LinkedList:
             self.insert_at_end(data)
 
 
-    def insert_after_value(self, data_after, data_to_insert):
-        if self.head is None:
-            return
-
-        if self.head.data==data_after:
-            self.head.next = Node(data_to_insert,self.head.next)
-            return
-
-        itr = self.head
-        while itr:
-            if itr.data == data_after:
-                itr.next = Node(data_to_insert, itr.next)
-                break
-
-            itr = itr.next
-
-    def remove_by_value(self, data):
-        if self.head is None:
-            return
-
-        if self.head.data == data:
-            self.head = self.head.next
-            return
-
-        itr = self.head
-        while itr.next:
-            if itr.next.data == data:
-                itr.next = itr.next.next
-                break
-            itr = itr.next
-
 if __name__ == '__main__':
-    ll = LinkedList()
+    ll = DoublyLinkedList()
     ll.insert_values(["banana","mango","grapes","orange"])
-    ll.print()
-    ll.insert_after_value("mango","apple")
-    ll.print()
-    ll.remove_by_value("orange")
-    ll.print()
-    ll.remove_by_value("figs")
-    ll.print()
-    ll.remove_by_value("banana")
-    ll.remove_by_value("mango")
-    ll.remove_by_value("apple")
-    ll.remove_by_value("grapes")
-    ll.print()
-
-    # ll.insert_values([45,7,12,567,99])
-    # ll.insert_at_end(67)
-    # ll.print()
+    ll.print_forward()
+    ll.print_backward()
+    ll.insert_at_end("figs")
+    ll.print_forward()
+    ll.insert_at(0,"jackfruit")
+    ll.print_forward()
+    ll.insert_at(6,"dates")
+    ll.print_forward()
+    ll.insert_at(2,"kiwi")
+    ll.print_forward()
 
